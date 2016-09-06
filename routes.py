29bb549a0141a421af52ab4e5af8111e3d714599
1002528 <1002528@SKP1002528MN001.local>
2016-09-06 15:14:52 +0900

    Merge branch 'master' of git://github.com/jee1/mirrorMirrorSmartMirror into jee1-master
    # Conflicts:
    #	routes.py
    #	templates/index.html

--- routes.py
+++ routes.py
@@ -8,6 +8,7 @@ import time
 from flask import Flask, redirect, url_for, render_template, request, Response
 #from gtts_token import gtts_token
 #import led
+import hangul_watch
 
 app = Flask(__name__)
 #domain = 'http://127.0.1.1:5000'
@@ -17,37 +18,66 @@ app = Flask(__name__)
 # "/"으로 접속시 templates 디렉토리의 index.html을 노출
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    now = time.localtime()
+    hangul_now = hangul_watch.convert_number_to_hangul((now.tm_year)) + '년 '
+    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_mon)) + '월 '
+    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_mday)) + '일 '
+    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_hour)) + '시 '
+    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_min)) + '분 '
+    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_sec)) + '초'
+
+    return render_template("index.html", hangul_now=hangul_now)
 
 # "/daumMedia.json"으로 접속시 daum내 뉴스 정보 호출하여 내용 일부 노출.
+
+
 @app.route("/daumMedia.json")
 def getDaumMedia():
     url = "http://media.daum.net/api/service/news/list/important/media.json"
     r = requests.get(url)
     return json.dumps(r.json()["simpleNews"][0])
 
+
+# 구글 TTS 를 통한 음성 출력
+@app.route("/gtts.json")
+def gtts():
+    text = request.args.get('text')
+    #url = 'https://translate.google.com/translate_tts?q=' + text + '&tl=ko&client=t&tk='
+    url = 'https://translate.google.com/translate_tts'
+    token = gtts_token.Token().calculate_token(text)    # 구글 TTS 를 사용하기 위한 token 생성
+    paramMap = {"q": text, "tl": "ko", "client": "t", "tk": token}
+
+    param = urllib.parse.urlencode(paramMap)
+    print(url + '?' + param)
+    r = requests.get(url + '?' + param)
+    resultObj = {"url": url + '?' + param}
+    return json.dumps(resultObj)
+    # return render_template("gtts.html", url=url + token)
+
+
 #  GPIO LED ON
 @app.route("/ledon.json")
 def ledon():
-#    led.ledon()
+    #    led.ledon()
     return json.dumps({"txt": "불을 켰습니다."})
 
 # GPIO LED OFF
+
+
 @app.route("/ledoff.json")
 def ledoff():
-#    led.ledoff()
+    #    led.ledoff()
     return json.dumps({"txt": "불을 껐습니다."})
 
 
-
 # 구글 TTS 를 사용하기 위한 token 생성
-#def generateGttsToken(text):
+# def generateGttsToken(text):
 #    token = gtts_token.Token().calculate_token(text)
 #    return token
 
 # 구글 TTS 를 통한 음성 출력
 #@app.route("/gtts.json")
-#def gtts():
+# def gtts():
 #    text = request.args.get('text')
 #    #url = 'https://translate.google.com/translate_tts?q=' + text + '&tl=ko&client=t&tk='
 #    url = 'https://translate.google.com/translate_tts'
