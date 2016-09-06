c05643366e3be70aa46ba23cba2d57d476826c39
1002528 <1002528@SKP1002528MN001.local>
2016-09-06 14:00:22 +0900

    no message

--- routes.py
+++ routes.py
@@ -6,12 +6,12 @@ import urllib
 import json
 import time
 from flask import Flask, redirect, url_for, render_template, request, Response
-from gtts_token import gtts_token
+#from gtts_token import gtts_token
 #import led
 
 app = Flask(__name__)
 #domain = 'http://127.0.1.1:5000'
-#domain = 'http://10.202.209.157:8000'
+#domain = 'http://localhost:8000'
 
 
 # "/"으로 접속시 templates 디렉토리의 index.html을 노출
@@ -19,37 +19,12 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-
 # "/daumMedia.json"으로 접속시 daum내 뉴스 정보 호출하여 내용 일부 노출.
 @app.route("/daumMedia.json")
 def getDaumMedia():
     url = "http://media.daum.net/api/service/news/list/important/media.json"
     r = requests.get(url)
     return json.dumps(r.json()["simpleNews"][0])
-
-
-# 구글 TTS 를 사용하기 위한 token 생성
-def generateGttsToken(text):
-    token = gtts_token.Token().calculate_token(text)
-    return token
-
-
-# 구글 TTS 를 통한 음성 출력
-@app.route("/gtts.json")
-def gtts():
-    text = request.args.get('text')
-    #url = 'https://translate.google.com/translate_tts?q=' + text + '&tl=ko&client=t&tk='
-    url = 'https://translate.google.com/translate_tts'
-    token = generateGttsToken(text)
-    paramMap = {"q": text, "tl": "ko", "client": "t", "tk": token}
-
-    param = urllib.parse.urlencode(paramMap)
-    print(url + '?' + param)
-    r = requests.get(url + '?' + param)
-    resultObj = {"url": url + '?' + param}
-    return json.dumps(resultObj)
-    # return render_template("gtts.html", url=url + token)
-
 
 #  GPIO LED ON
 @app.route("/ledon.json")
@@ -62,6 +37,30 @@ def ledon():
 def ledoff():
 #    led.ledoff()
     return json.dumps({"txt": "불을 껐습니다."})
+
+
+
+# 구글 TTS 를 사용하기 위한 token 생성
+#def generateGttsToken(text):
+#    token = gtts_token.Token().calculate_token(text)
+#    return token
+
+# 구글 TTS 를 통한 음성 출력
+#@app.route("/gtts.json")
+#def gtts():
+#    text = request.args.get('text')
+#    #url = 'https://translate.google.com/translate_tts?q=' + text + '&tl=ko&client=t&tk='
+#    url = 'https://translate.google.com/translate_tts'
+#    token = generateGttsToken(text)
+#    paramMap = {"q": text, "tl": "ko", "client": "t", "tk": token}
+#
+#    param = urllib.parse.urlencode(paramMap)
+#    print(url + '?' + param)
+#    r = requests.get(url + '?' + param)
+#    resultObj = {"url": url + '?' + param}
+#    return json.dumps(resultObj)
+#    # return render_template("gtts.html", url=url + token)
+#
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
