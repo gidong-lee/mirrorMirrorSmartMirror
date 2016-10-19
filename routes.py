cee12216ee903b15932e422b8033159b74e39510
gidong.lee <gidong.lee@sk.com>
2016-10-19 13:42:44 +0900

    날씨, 유투브

--- routes.py
+++ routes.py
@@ -1,32 +1,24 @@
 # -*- coding: UTF-8 -*-
 
+#from gtts_token import gtts_token
+#import led
+
 import logging
 import requests
 import urllib
 import json
 import time
 from flask import Flask, redirect, url_for, render_template, request, Response
-#from gtts_token import gtts_token
-#import led
-import hangul_watch
+import configparser
 
 app = Flask(__name__)
-#domain = 'http://127.0.1.1:5000'
-#domain = 'http://localhost:8000'
-
+config = configparser.ConfigParser()
+config.read("settings.cfg")
 
 # "/"으로 접속시 templates 디렉토리의 index.html을 노출
 @app.route("/")
 def hello():
-    now = time.localtime()
-    hangul_now = hangul_watch.convert_number_to_hangul((now.tm_year)) + '년 '
-    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_mon)) + '월 '
-    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_mday)) + '일 '
-    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_hour)) + '시 '
-    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_min)) + '분 '
-    hangul_now += hangul_watch.convert_number_to_hangul((now.tm_sec)) + '초'
-
-    return render_template("index.html", hangul_now=hangul_now)
+    return render_template("index2.html")
 
 # "/daumMedia.json"으로 접속시 daum내 뉴스 정보 호출하여 내용 일부 노출.
 
@@ -38,22 +30,27 @@ def getDaumMedia():
     return json.dumps(r.json()["simpleNews"][0])
 
 
-# 구글 TTS 를 통한 음성 출력
-@app.route("/gtts.json")
-def gtts():
+@app.route("/youtube.json")
+def getyoutubeMedia():
     text = request.args.get('text')
-    #url = 'https://translate.google.com/translate_tts?q=' + text + '&tl=ko&client=t&tk='
-    url = 'https://translate.google.com/translate_tts'
-    token = gtts_token.Token().calculate_token(text)    # 구글 TTS 를 사용하기 위한 token 생성
-    paramMap = {"q": text, "tl": "ko", "client": "t", "tk": token}
+    url = "https://www.googleapis.com/youtube/v3/search?key=" + \
+        config["DEFAULT"]["YOUTUBE_APP_KEY"] + \
+        "&q=" + text + "&maxResults=6&part=snippet"
+    r = requests.get(url)
+    return json.dumps(r.json())
 
-    param = urllib.parse.urlencode(paramMap)
-    print(url + '?' + param)
-    r = requests.get(url + '?' + param)
-    resultObj = {"url": url + '?' + param}
-    return json.dumps(resultObj)
-    # return render_template("gtts.html", url=url + token)
 
+@app.route("/weatherInfo")
+def getWeatherInfo():
+    longitude = request.args.get('longitude')
+    latitude = request.args.get('latitude')
+
+    headers = {'appKey': config["DEFAULT"]["SKPLANET_APP_KEY"]}
+    url = 'http://apis.skplanetx.com/weather/current/hourly?version=1&lat=' + (str(latitude))+ '&lon=' + (str(longitude))
+    
+
+    r = requests.get(url, headers=headers)
+    return json.dumps(r.json()["weather"]["hourly"][0])
 
 #  GPIO LED ON
 @app.route("/ledon.json")
@@ -91,6 +88,21 @@ def ledoff():
 #    return json.dumps(resultObj)
 #    # return render_template("gtts.html", url=url + token)
 #
+# 구글 TTS 를 통한 음성 출력
+@app.route("/gtts.json")
+def gtts():
+    text = request.args.get('text')
+    #url = 'https://translate.google.com/translate_tts?q=' + text + '&tl=ko&client=t&tk='
+    url = 'https://translate.google.com/translate_tts'
+    token = gtts_token.Token().calculate_token(text)    # 구글 TTS 를 사용하기 위한 token 생성
+    paramMap = {"q": text, "tl": "ko", "client": "t", "tk": token}
+
+    param = urllib.parse.urlencode(paramMap)
+    print(url + '?' + param)
+    r = requests.get(url + '?' + param)
+    resultObj = {"url": url + '?' + param}
+    return json.dumps(resultObj)
+    # return render_template("gtts.html", url=url + token)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
---
+++ static/js/customMain.js
@@ -0,0 +1,259 @@
+/* ========================================================================= */
+/*  Preloader
+/* ========================================================================= */
+jQuery(window).load(function() {});
+/* ========================================================================= */
+/*  Welcome Section Slider
+/* ========================================================================= */
+$(function() {});
+$(document).ready(function() {
+    var userList = [{
+        'username': "이기동"
+    }, {
+        'username': "김현정"
+    }, {
+        'username': "이지원"
+    }, {
+        'username': "김주영"
+    }, {
+        'username': "김나래"
+    }, {
+        'username': "박동조"
+    }, {
+        'username': "박성욱"
+    }, {
+        'username': "양중근"
+    }];
+    $('#userTmpl').tmpl(userList).appendTo("#userListEl");
+});
+
+
+function call_tts(text) {
+    $("#talk").text(text);
+    var parameters = {
+        onstart: startTTSCallback,
+        onend: voiceEndCallback
+    }
+    //responsiveVoice.speak(text, "Korean Female", parameters);
+}
+
+function startTTSCallback() {
+    console.log("startTTS");
+    //annyang.pause();
+}
+
+function voiceEndCallback() {
+    setInterval(function() {
+        //annyang.resume();
+    }, 2000);
+    console.log("endTTS");
+}
+
+function showMaker(){
+    $("a[href='#MAKER']").trigger("click");
+}
+
+function closeYoutube(){
+    if($(".fancybox-close")){
+        call_tts('동영상 종료');
+        $(".fancybox-close").trigger("click");
+    }
+}
+
+function showYouTubeNum(num){
+    var movieNum = 0;
+    switch(num){
+        case '첫':
+        case '1':
+            movieNum = 1;break;
+        case '두':
+        case '2':
+            movieNum = 2;break;
+        case '세':
+        case '3':
+            movieNum = 3;break;
+        case '네':
+        case '4':
+            movieNum = 4;break;
+        case '다섯':
+        case '5':
+            movieNum = 5;break;
+        case '여섯':
+        case '6':
+            movieNum = 6;break;
+    }
+    if(movieNum){
+        call_tts(movieNum + '번째 동영상 실행');
+        $(("#videoId" + movieNum)).trigger("click");
+    }
+   
+}
+
+function showYouTube(title) {
+    var url = "youtube.json";
+    
+    call_tts(title + ' 동영상 검색');
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        data: {
+            'text': title
+        },
+        success: function(data) {
+            var items = data.items;
+            //$("#youtubeDiv")[0].src = "http://www.youtube.com/embed/"+data.items[0]["id"].videoId+"?autoplay=1&origin=http://127.0.0.1";
+
+            call_tts(data.items.length + '개 동영상이 검색 되었습니다.');
+
+            var youtubeList = [];
+            for (var mIdx = 0; mIdx < data.items.length; mIdx++) {
+                var tempItem = {};
+                var youtubeItem = data.items[mIdx];
+                tempItem.index = mIdx + 1;
+                tempItem.videoId = youtubeItem.id.videoId;
+                tempItem.title = youtubeItem.snippet.title;
+                tempItem.imageUrl = youtubeItem.snippet.thumbnails.high.url;
+
+                youtubeList.push(tempItem);
+            }
+            $("#youtubeListEl").empty();
+            $('#youtubeTmpl').tmpl(youtubeList).appendTo("#youtubeListEl");
+            $("a[href='#YOUTUBE']").trigger("click");
+
+        }
+    });
+}
+
+function ledControl(status) {
+    var url;
+    if (status == true) {
+        url = "/ledon.json"
+    } else {
+        url = "/ledoff.json"
+    }
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        data: {},
+        success: function(data) {
+            if (data.txt) {
+                call_tts(data.txt);
+            }
+        }
+    });
+}
+
+function showLotto() {
+    var lotto = new Array(6);
+    var count = 0;
+    var mFlag = true;
+    while (count < 6) {
+        var number;
+        number = parseInt(Math.random() * 45) + 1
+        for (var i = 0; i < count; i++)
+            if (lotto[i] == number) mFlag = false;
+        if (mFlag) {
+            lotto[count] = number;
+            count++;
+        }
+        mFlag = true;
+    }
+    // 추첨된 로또번호 출력
+    call_tts('대박나세요 ' + lotto[0] + ' ' + lotto[1] + ' ' + lotto[2] + ' ' + lotto[3] + ' ' + lotto[4] + ' ' + lotto[5] + "번");
+}
+
+function showTodayWeather(){
+    var latitude;
+    var longitude;
+    
+    navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      
+      var url = "/weatherInfo?latitude="+latitude+"&longitude="+longitude;
+
+      $.ajax({
+          url: url,
+          dataType: 'json',
+          data : {
+            latitude : latitude,
+            longitude : longitude
+          },
+          success: function(weatherInfo) {
+            var tts_text = weatherInfo.grid.city + ' ' + weatherInfo.grid.county + ' 날씨는 ' + weatherInfo.sky.name + '입니다';
+            call_tts(tts_text);
+            $("#weatherEl").empty();
+            //$('#weatherTmpl').tmpl(youtubeList).appendTo("#weatherEl");
+            $("a[href='#WEATHER']").trigger("click");
+
+
+            //$("#weatherText").text(weatherInfo);
+          }
+      });
+    });
+}
+/*weatherInfo.sky.code
+"SKY_O07"
+weatherInfo.sky.name
+"흐림"
+weatherInfo.temperature.tc
+"18.90"
+weatherInfo.temperature.tmax
+"22.00"
+weatherInfo.temperature.tmin
+"15.00"
+*/
+
+window.onload = function() {
+    if (annyang) {
+        var commandsDefault = {
+            '*talk': function(talk) {
+                call_tts(talk);
+            }
+        };
+        var commands = {
+            '거울아 :userName 보여줘': function(userName) {
+                call_tts(userName + ' 일어났습니다.');
+            },
+            '거울아 :title 동영상 검색': function(title) {
+                showYouTube(title);
+            },
+            ':num번째 동영상 실행': function(num) {
+                showYouTubeNum(num);
+            },
+            '동영상 종료': function(num) {
+                closeYoutube();
+            },
+            '거울아 만든 사람들 보여 줘': function(num) {
+                showMaker();
+            },
+            '거울아 오늘 날씨*': function() {
+                showTodayWeather();
+            },'거울아 날씨': function() {
+                showTodayWeather();
+            },
+            '야 불 켜': function() {
+                //call_tts("야 불 켜");
+                ledControl(true);
+            },
+            '야 불 꺼': function() {
+                //call_tts("야 불 꺼");
+                ledControl(false);
+            },
+            '거울아 로또 번호 좀': function() {
+                showLotto();
+            }
+        };
+        //annyang.setLanguage("en-US");
+        annyang.setLanguage("ko");
+        //annyang.addCommands(commandsDefault);
+        annyang.addCommands(commands);
+        annyang.debug(true);
+        annyang.addCallback('resultNoMatch', function(userSaid) {
+            $("#talk").text("not match : " + userSaid);
+            console.log("userSaid : " + userSaid); // sample output: 'hello'
+        });
+        annyang.start({});
+    }
+}
