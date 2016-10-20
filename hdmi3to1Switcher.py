13d9867edac4ea16891b19881fa50b8cc3a2fd10
jee1lee <jee1lee@gmail.com>
2016-10-20 18:46:25 +0900

    FREQUENCY 의 자료형을 딕셔너리로 변경

--- hdmi3to1Switcher.py
+++ hdmi3to1Switcher.py
@@ -8,9 +8,9 @@ Created on 2016. 10. 20.
 import RPi.GPIO as GPIO
 
 CHANNEL = 17 # 적외선 송신기 연결 PIN번호
-FREQUENCY = [[1, 0.5],  # 리모컨의 각 번호에 맞는 버튼의 frequency 를 설정한다. 
-             [2, 1],
-             [3, 1.5]] 
+FREQUENCY = {1: 0.5,  # 리모컨의 각 번호에 맞는 버튼의 frequency 를 설정한다. 
+             2: 1,
+             3: 1.5}
 
 GPIO.setmode(GPIO.BOARD)    # GPIO 로 시작하는 번호가 아니라, 보드자체의 핀번호를 사용한다. 
 GPIO.setup(CHANNEL, GPIO.OUT)
